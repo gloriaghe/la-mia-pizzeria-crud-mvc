b50295570e36f511e9db8b3bb12d2dcd660986e9
gloriaghe <gloghe@yahoo.it>
2022-11-22 15:43:03 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,52 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        PizzaDbContext db;
+
+        public PizzaController() : base()
+        {
+            db = new PizzaDbContext();
+        }
+        public IActionResult Index()
+        {
+            List<Pizza> listaPizze = db.Pizzas.ToList();
+            return View(listaPizze);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Pizza pizza = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+            if (pizza == null)
+                return View("Errore", "Pizza non trovata");
+
+            return View(pizza);
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            db.Pizzas.Add(pizza);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,42 @@
+﻿using la_mia_pizzeria_static.Validation;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il titolo è obbligatorio")]
+        [StringLength(80, ErrorMessage = "Il titolo non può essere oltre 80 caratteri")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "text")]
+        [Required(ErrorMessage = "La descrizione è obbligatoria")]
+        [StringLength(250, ErrorMessage = "La descrizione non può superare i 250 caratteri")]
+        [CinqueParoleDescrizione]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "L'immagine è obbligatoria")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Il prezzo è obbligatorio")]
+        [Range(0, 200, ErrorMessage = "Il prezzo deve essere compreso fra 0 e 200")]
+        public double Price { get; set; }
+
+        public Pizza(string name, string description, string image, double price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+        public Pizza()
+        {
+        }
+
+    }
+}
